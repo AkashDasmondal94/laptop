b18a1b6b5aad2f76a5382c6031d9ed76c0663db6
AkashDasmondal94 <103578727+AkashDasmondal94@users.noreply.github.com>
2022-09-11 22:41:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Dashboard } from "./Components/Dashboard";
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import EmployeeName from "./Components/EmployeeName";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
